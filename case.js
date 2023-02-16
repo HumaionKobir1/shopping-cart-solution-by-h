3da3b34bf418c97d72c00dab4ac5e0e11fa7d40f
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-17 01:10:17 +0600

    Shopping cart handle decrease and update price

---
+++ case.js
@@ -0,0 +1,36 @@
+/**
+ * 1. add event listener to the case plus button
+ * 2. get the value inside the case n umber field (input field)
+ * 3. convert the number to an integer
+ * 4. calculate the nre case number
+ * 5. set the value to the number field
+ */
+
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseFloat(caseNumberString);
+    let newCaseNumber;
+
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber
+}
+
+function btnCasePlus(){
+    const newCaseNumber = updateCaseNumber(true);
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+    
+}
+
+function btnCaseMinus(){
+    updateCaseNumber(false);
+}
+
